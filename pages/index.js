ae7b1a8a04e24d81848b5281e1254616ef1b611f
Nidhish Chauhan <rch246chauhan@gmail.com>
2023-04-11 23:41:41 +0530

    user conditional rendering added

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,28 @@
 import Link from "next/link";
 import React from "react";
+import { UserProvider, useUser } from "@auth0/nextjs-auth0/client";
+import Image from "next/image";
 
 const Home = () => {
+  const { user } = useUser();
+
+  console.log(user);
   return (
     <div>
-      <Link href={"/api/auth/login"}>Login </Link>
+      {user ? (
+        <div>
+          <Image
+            src={user.picture}
+            alt={user.name}
+            height={50}
+            width={50}
+          ></Image>
+          <div>{user.email}</div>
+          <Link href={"/api/auth/logout"}>Logout </Link>
+        </div>
+      ) : (
+        <Link href={"/api/auth/login"}>Login </Link>
+      )}
     </div>
   );
 };
